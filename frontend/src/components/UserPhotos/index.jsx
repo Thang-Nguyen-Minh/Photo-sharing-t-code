327148b61a2609ef79899902b8ae1062bf23ca26
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-06-05 14:11:40 +0700

    2 map thì map thứ 2 vẫn ở trong return của map 1 và phải là photo.comments.map

--- frontend/src/components/UserPhotos/index.jsx
+++ frontend/src/components/UserPhotos/index.jsx
@@ -1,14 +1,31 @@
 import React, {useEffect,useState} from "react";
-import {Card, CardContent, ListItem, Typography} from "@mui/material";
+import {Card, CardContent, ListItem, Typography,Button} from "@mui/material";
 import "./styles.css";
 import {useParams,Link} from "react-router-dom";
 import models from "../../modelData/models";
 
 function UserPhotos () {
-
+    const {userId} = useParams();
+    const photos = models.photoOfUserModel(userId);
     return (
         <div>
-
+            {photos.map(photo => {
+                return (
+                    <Card>
+                        <img src={`/images/${photo.file_name}`} alt={photo.file_name} />
+                        <Typography>{photo.date_time}</Typography>
+                        {photo.comments && photo.comments.map(comment => (
+                            <div>
+                                <Link to={`/users/${comment.user._id}`}>
+                                    {comment.user.first_name} {comment.user.last_name}
+                                </Link>
+                                <p>{comment.date_time}</p>
+                                <p>{comment.comment}</p>
+                            </div>
+                        ))}
+                    </Card>
+                )
+            })}
         </div>
     );
 }
