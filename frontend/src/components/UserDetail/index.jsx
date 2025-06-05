e28d2d2c7bf3b7869b7a48f202df7c9346db07e0
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-06-05 13:41:43 +0700

    Cái userId lấy ra với useParams phải đúng với cái trong Route định nghĩa UserDeatils thì mới được,nói chung chỉ có thể thôi

--- frontend/src/components/UserDetail/index.jsx
+++ frontend/src/components/UserDetail/index.jsx
@@ -6,10 +6,31 @@ import UserList from "../UserList";
 import models from "../../modelData/models";
 
 function UserDetail() {
-
+    const {userId} = useParams();
+    const users = models.userModel(userId);
     return (
         <div>
-
+            <Card>
+                <CardContent>
+                    <Typography variant="h5">User Information</Typography>
+                    <Typography variant="body1"><strong>Name : </strong>
+                        {users.first_name} {users.last_name}
+                    </Typography>
+                    <Typography variant="body1"><strong>Location : </strong>
+                        {users.location} 
+                    </Typography>
+                    <Typography variant="body1"><strong>Description : </strong>
+                        {users.description} 
+                    </Typography>
+                    <Typography variant="body1"><strong>Occupation : </strong>
+                        {users.occupation} 
+                    </Typography>
+                    <br/>
+                    <Link to={`/photos/${users._id}`}>
+                        <Button>PHOTOS</Button>
+                    </Link>
+                </CardContent>
+            </Card>
         </div>
     );
 }
