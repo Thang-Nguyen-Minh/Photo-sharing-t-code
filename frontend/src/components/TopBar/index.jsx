4407766a5784eda726461d5a6cbd9851b2ba7cad
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-06-05 14:21:26 +0700

    Solve TopBar với location , usestate và useEffect thay đổi theo location

--- frontend/src/components/TopBar/index.jsx
+++ frontend/src/components/TopBar/index.jsx
@@ -2,8 +2,22 @@ import React, { useEffect, useState } from "react";
 import { AppBar, Toolbar, Typography } from "@mui/material";
 import { useLocation } from "react-router-dom";
 import "./styles.css";
-
+import models from "../../modelData/models";
 function TopBar() {
+    const [contextText, setContextText] = useState("");
+    const location = useLocation();
+    const path=location.pathname;
+
+    useEffect(() => {
+        const x=path.split("/");
+        const id=x[2];
+        if (x[1]==="users" || x[1]==="photos"){
+            const users=models.userModel(id);
+            const name=`${users.first_name} ${users.last_name}`
+            if (x[1]==="photos") setContextText(`Photos of ${name}`);
+            else setContextText(`${name}`);
+        }
+    },[location]);
 
     return (
         <AppBar className="topbar-appBar" position="absolute">
@@ -12,7 +26,7 @@ function TopBar() {
                     Photo Sharing App
                 </Typography>
                 <Typography variant="h5" color="inherit" style={{ marginLeft: "auto" }}>
-                    {/*{contextText}*/}
+                    {contextText}
                 </Typography>
             </Toolbar>
         </AppBar>
